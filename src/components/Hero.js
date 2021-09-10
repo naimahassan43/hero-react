97c8d4382538fa0c2c7bae91ad49fd6e1f7c26fa
Naima Hassan <naima.hassan434@gmail.com>
2021-09-10 21:07:43 +0100

    Hero list using Array

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,13 +4,60 @@ import "tachyons";
 import Herolist from "./HeroList.js";
 
 const Hero = (props) => {
+  const herolistArray = [
+    {
+      id: 1,
+      fullname: "John Smith",
+      name: "jon",
+      work: "Web Developer",
+    },
+    {
+      id: 2,
+      fullname: "Rajon Chowdhury",
+      name: "raki",
+      work: "Graphic Designer",
+    },
+    {
+      id: 3,
+      fullname: "Milon Hasan",
+      name: "miln",
+      work: "Network Engineer",
+    },
+    {
+      id: 4,
+      fullname: "Rahul Das",
+      name: "rahi",
+      work: "Front End Developer",
+    },
+  ];
+
   return (
     <div>
       <h1>Welcome to Hero World</h1>
-      <Herolist id="1" name="John Smith" work="web developer" />
-      <Herolist id="2" name="Raj Chowdhury" work="web developer" />
-      <Herolist id="3" name="Milon Hasan" work="web developer" />
-      <Herolist id="4" name="Rahul Das" work="web developer" />
+      <Herolist
+        id={herolistArray[0].id}
+        fullname={herolistArray[0].fullname}
+        name={herolistArray[0].name}
+        work={herolistArray[0].work}
+      />
+      <Herolist
+        id={herolistArray[1].id}
+        fullname={herolistArray[1].fullname}
+        name={herolistArray[1].name}
+        work={herolistArray[1].work}
+      />
+      <Herolist
+        id={herolistArray[2].id}
+        fullname={herolistArray[2].fullname}
+        name={herolistArray[2].name}
+        work={herolistArray[2].work}
+      />
+      <Herolist
+        id={herolistArray[3].id}
+        fullname={herolistArray[3].fullname}
+        name={herolistArray[3].name}
+        work={herolistArray[3].work}
+      />
 
       <button>Subscribe</button>
     </div>
--- src/components/HeroList.js
+++ src/components/HeroList.js
@@ -4,7 +4,7 @@ const Herolist = (props) => {
   return (
     <div className="hero-style bg-light-purple ma4 dib pa4">
       <img src={`https://joeschmoe.io/api/v1/${props.name}`} alt="John" />
-      <h1>{props.name}</h1>
+      <h1>{props.fullname}</h1>
       <p>{props.work}</p>
     </div>
   );
