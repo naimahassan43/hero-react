be87c6ea5d7ee4751fd747d7e1e079d3c99ef202
Naima Hassan <naima.hassan434@gmail.com>
2021-09-10 22:17:08 +0100

    Convert funtional to class component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,53 +3,59 @@ import "./Hero.css";
 import "tachyons";
 import Herolist from "./HeroList.js";
 
-const Hero = (props) => {
-  const herolistArray = [
-    {
-      id: 1,
-      fullname: "John Smith",
-      name: "jon",
-      work: "Web Developer",
-    },
-    {
-      id: 2,
-      fullname: "Rajon Roy",
-      name: "raki",
-      work: "Graphic Designer",
-    },
-    {
-      id: 3,
-      fullname: "Milon Hasan",
-      name: "miln",
-      work: "Network Engineer",
-    },
-    {
-      id: 4,
-      fullname: "Rahul Das",
-      name: "rahi",
-      work: "Front End Developer",
-    },
-  ];
+class Hero extends React.Component {
+  render() {
+    const herolistArray = [
+      {
+        id: 1,
+        fullname: "John Smith",
+        name: "jon",
+        work: "Web Developer",
+      },
+      {
+        id: 2,
+        fullname: "Rajon Roy",
+        name: "raki",
+        work: "Graphic Designer",
+      },
+      {
+        id: 3,
+        fullname: "Milon Hasan",
+        name: "miln",
+        work: "Network Engineer",
+      },
+      {
+        id: 4,
+        fullname: "Rahul Das",
+        name: "rahi",
+        work: "Front End Developer",
+      },
+    ];
 
-  const arrayHeroCard = herolistArray.map((herocard, i) => {
+    const arrayHeroCard = herolistArray.map((herocard, i) => {
+      return (
+        <Herolist
+          id={herolistArray[i].id}
+          fullname={herolistArray[i].fullname}
+          name={herolistArray[i].name}
+          work={herolistArray[i].work}
+        />
+      );
+    });
+
     return (
-      <Herolist
-        id={herolistArray[i].id}
-        fullname={herolistArray[i].fullname}
-        name={herolistArray[i].name}
-        work={herolistArray[i].work}
-      />
+      <div className="main-page">
+        <h1>Welcome to Hero World</h1>
+        <div>{arrayHeroCard}</div>
+
+        <button>Subscribe</button>
+      </div>
     );
-  });
+  }
+}
 
-  return (
-    <div className="main-page">
-      <h1>Welcome to Hero World</h1>
-      {arrayHeroCard}
+// const Hero = (props) => {
 
-      <button>Subscribe</button>
-    </div>
-  );
-};
+// };
 
 export default Hero;
