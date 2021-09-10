156d06963df388df6045c9aae2b6b4096a36a2b0
Naima Hassan <naima.hassan434@gmail.com>
2021-09-10 21:45:13 +0100

    Add CSS style

--- src/components/HeroList.js
+++ src/components/HeroList.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Herolist = (props) => {
   return (
-    <div className="hero-style bg-light-purple ma4 dib pa4">
+    <div className="hero-style bg-light-purple ma4 dib pa3 grow shadow-4 tc">
       <img src={`https://joeschmoe.io/api/v1/${props.name}`} alt="John" />
       <h1>{props.fullname}</h1>
       <p>{props.work}</p>
